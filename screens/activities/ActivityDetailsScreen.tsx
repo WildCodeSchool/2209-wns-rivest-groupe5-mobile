80a9786872bd38cb940dc1de21f7c1e84bb1205a
witzkvn <witz.kevin05@gmail.com>
2023-01-29 18:05:38 +0100

    ui card activity, activityDetailsPage

--- screens/activities/ActivityDetailsScreen.tsx
+++ screens/activities/ActivityDetailsScreen.tsx
@@ -1,12 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { IActivity } from "../../interfaces/IActivity";
+import { formatDateToDDMMYY } from "../../helpers/formatter";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const ActivityDetailsScreen = () => {
+const ActivityDetailsScreen = ({ route, navigation }) => {
+  const { activity }: { activity: IActivity } = route.params;
   return (
-    <View>
-      <Text>ActivityDetailsScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>{activity.title}</Text>
+      <Text style={styles.type}>{activity.activityType.name}</Text>
+      <Text style={styles.date}>
+        Activité effectuée le {formatDateToDDMMYY(activity.activityDate)}
+      </Text>
+      <Text style={styles.carbon}>{activity.carbonQuantity} kg de CO2</Text>
+      <Text style={styles.description}>{activity.description}</Text>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    flex: 1,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  type: {
+    fontStyle: "italic",
+    color: "#888",
+    textAlign: "center",
+  },
+  date: { textAlign: "center" },
+  carbon: { textAlign: "center", marginBottom: 30 },
+  description: {
+    marginBottom: 50,
+    lineHeight: 22,
+  },
+});
 
 export default ActivityDetailsScreen;
--- screens/activities/activitiesTab/MyActivitiesScreen.tsx
+++ screens/activities/activitiesTab/MyActivitiesScreen.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React, { useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { GET_MY_ACTIVITIES } from "../../../graphql/queries/activities/getMyActivitiesQuery";
 import { IActivity } from "../../../interfaces/IActivity";
 import { useFocusEffect } from "@react-navigation/native";
 import { useIsFocused } from "@react-navigation/native";
+import { formatDateToDDMMYY } from "../../../helpers/formatter";
+import { ScrollView } from "react-native-gesture-handler";
 
-const MyActivitiesScreen = () => {
+const MyActivitiesScreen = ({ navigation }) => {
   const [getMyActivities, { loading, error }] = useLazyQuery(GET_MY_ACTIVITIES);
   const [acitivities, setActivities] = useState([]);
   const isFocused = useIsFocused();
@@ -47,15 +49,66 @@ const MyActivitiesScreen = () => {
   }
 
   return (
-    <View>
+    <ScrollView>
       {acitivities &&
         acitivities.map((activity: IActivity) => {
-          console.log(activity);
-          return <Text key={activity.activityId}>{activity.title}</Text>;
+          return (
+            <Pressable
+              key={activity.activityId}
+              style={styles.card}
+              onPress={() => {
+                navigation.navigate("ActivityDetails", {
+                  activity: activity,
+                });
+              }}
+            >
+              <Text style={styles.title}>{activity.title}</Text>
+              <Text style={styles.type}>{activity.activityType.name}</Text>
+              <View style={styles.bottom}>
+                <Text style={styles.carbon}>
+                  {activity.carbonQuantity} kg de CO2
+                </Text>
+                <Text style={styles.date}>
+                  {formatDateToDDMMYY(activity.activityDate)}
+                </Text>
+              </View>
+            </Pressable>
+          );
         })}
-      <Text>MyActivitiesScreen</Text>
-    </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 5,
+    padding: 15,
+    backgroundColor: "white",
+    margin: 5,
+    shadowColor: "rgba(50,50,50,0.6)",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  type: {
+    fontStyle: "italic",
+    color: "#888",
+    marginBottom: 15,
+  },
+  bottom: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 5,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+  carbon: { fontSize: 12 },
+  date: { fontSize: 12 },
+});
 
 export default MyActivitiesScreen;
